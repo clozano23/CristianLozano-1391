084cdba7503c90a759485e88862c2c7c1928fceb
clozano23 <107647959+clozano23@users.noreply.github.com>
2023-05-10 21:45:11 -0500

    actualizacion
    se realiza la trascripcion del log para el cuestionario, se hace correccion en alguna cosas, esta funcional, falta ingresar la investigacion

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 function procesarRespuestas() {
-    let total = 3
+    let total = 5;
     let puntos = 0;
 
-    let myform = document.forms["formulario"];
-    let respuestasCorrectas = ["c", "a", "b"];
+    let myform = document.forms["cuestionario"];
+    let respuestasCorrectas = ["c","a","b","c","a"];
 
-    for (let i = 1 i <= total=; i++) {
+    for (let i =1; i <= total; i++) {    
         if (myform["r" + i].value == null ||
-            myform["r" + i].value == "") {
-            alert('Favor responder todas las preguntras')
+            myform["r" + i].value == '') {
+            alert('Favor responder todas las preguntas');
             return false;
         } else {
             if (myform["r" + i].value === respuestasCorrectas[i - 1])
-                puntos++;
+                puntos++
         }
     }
-    let resultado = document.getElementById('RESULTADO')
-    resultado.interHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML='Obtuviste ' + puntos + ' puntos de ' + total + ' posibles ';
     return false;
 }
